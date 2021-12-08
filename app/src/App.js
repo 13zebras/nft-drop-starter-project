f24d239161ec702a704edd137107514d39b10dea
Tom <13zebras@users.noreply.github.com>
2021-12-07 23:01:31 -0600

    modified CandyMachine and App to Section 3.1

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import CandyMachine from './CandyMachine';
 
 // Constants
 const TWITTER_HANDLE = 'tom13zebras';
@@ -92,9 +93,11 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
-		  {/* Add the condition to show this only if we don't have a wallet address */}
+		  
           {!walletAddress && renderNotConnectedContainer()}
         </div>
+		
+		{walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import { MintLayout, TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
@@ -9,6 +9,7 @@ import {
   TOKEN_METADATA_PROGRAM_ID,
   SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
 } from './helpers';
+
 const {
   metadata: { Metadata, MetadataProgram },
 } = programs;
@@ -25,6 +26,10 @@ const MAX_SYMBOL_LENGTH = 10;
 const MAX_CREATOR_LEN = 32 + 1 + 1;
 
 const CandyMachine = ({ walletAddress }) => {
+  
+  // State
+  const [machineStats, setMachineStats] = useState(null);
+
   // Actions
   const fetchHashTable = async (hash, metadataEnabled) => {
     const connection = new web3.Connection(
@@ -250,11 +255,69 @@ const CandyMachine = ({ walletAddress }) => {
     });
   };
 
+  useEffect(() => {
+    getCandyMachineState();
+  }, []);
+
+  const getProvider = () => {
+	const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST;
+	
+	const connection = new Connection(rpcHost);
+	
+	const provider = new Provider(
+	  connection,
+	  window.solana,
+	  opts.preflightCommitment
+	);
+  
+	return provider;
+  };
+
+  // Declare getCandyMachineState as an async method
+  const getCandyMachineState = async () => { 
+	const provider = getProvider();
+	
+	const idl = await Program.fetchIdl(candyMachineProgram, provider);
+  
+	
+	const program = new Program(idl, candyMachineProgram, provider);
+  
+	const candyMachine = await program.account.candyMachine.fetch(
+	  process.env.REACT_APP_CANDY_MACHINE_ID
+	);
+	
+	const itemsAvailable = candyMachine.data.itemsAvailable.toNumber();
+	const itemsRedeemed = candyMachine.itemsRedeemed.toNumber();
+	const itemsRemaining = itemsAvailable - itemsRedeemed;
+	const goLiveData = candyMachine.data.goLiveDate.toNumber();
+  
+	// We will be using this later in our UI so let's generate this now
+	const goLiveDateTimeString = `${new Date(
+	  goLiveData * 1000
+	).toGMTString()}`
+
+	setMachineStats({
+		itemsAvailable,
+		itemsRedeemed,
+		itemsRemaining,
+		goLiveData,
+		goLiveDateTimeString,
+	});
+  
+	console.log({
+	  itemsAvailable,
+	  itemsRedeemed,
+	  itemsRemaining,
+	  goLiveData,
+	  goLiveDateTimeString,
+	});
+  };
+
   return (
     <div className="machine-container">
-      <p>Drop Date:</p>
-      <p>Items Minted:</p>
-      <button className="cta-button mint-button" onClick={mintToken}>
+      <p>{`Drop Date: ${machineStats.goLiveDateTimeString}`}</p>
+      <p>{`Items Minted: ${machineStats.itemsRedeemed} / ${machineStats.itemsAvailable}`}</p>
+      <button className="cta-button mint-button">
         Mint NFT
       </button>
     </div>
